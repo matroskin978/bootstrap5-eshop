28541a349a8d01b3480f4773a9f891214654f736
Админ <matroskin8@i.ua>
2023-05-05 11:16:45 +0300

    Lesson 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 window.addEventListener('scroll', function () {
     document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
 });
+
+$(document).ready(function () {
+    $(".owl-carousel-full").owlCarousel({
+        margin: 20,
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 2
+            },
+            700: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
