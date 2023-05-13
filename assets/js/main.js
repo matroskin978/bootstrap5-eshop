cb950bc7a3a302d5880d62387c4ae0bde6d65da7
Админ <matroskin8@i.ua>
2023-05-13 14:04:30 +0300

    Lesson 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,35 @@ window.addEventListener('scroll', function () {
     document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
 });
 
+const offcanvasCartEl = document.getElementById('offcanvasCart');
+const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+
+document.querySelectorAll('.closecart').forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        offcanvasCart.hide();
+        // let href = item.href.split('#').pop();
+        const href = item.dataset.href;
+        offcanvasCartEl.addEventListener('hidden.bs.offcanvas', () => {
+            document.getElementById(href).scrollIntoView();
+        });
+    });
+});
+
 $(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    });
+
+    $('#top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
     $(".owl-carousel-full").owlCarousel({
         margin: 20,
         responsive: {
